fac63aa62ddbdd4bf9529f1f8874550b33f0633b
fredlynx <fredlynx@hotmail.com>
2016-11-07 16:54:23 +0100

    Update BoxWithBorder.cpp

--- BoxWithBorder/BoxWithBorder.cpp
+++ BoxWithBorder/BoxWithBorder.cpp
@@ -38,6 +38,46 @@ void touchgfx::BoxWithBorder::draw(const touchgfx::Rect& invalidatedArea) const
 	bool inBorder;
 	uint8_t out_r, out_g, out_b;
 	uint16_t *framebuffer = touchgfx::HAL::getInstance()->lockFrameBuffer();
+/******************************************************************************
+*
+* @brief     This file is a Box widget for TouchGFX.
+*
+* @author    Fredlynx
+*
+******************************************************************************/
+
+#include <gui\common\BoxWithBorder.hpp>
+
+
+/**
+ * @fn touchgfx::Rect BoxWithBorder::getSolidRect() const;
+ *
+ * @brief Get the solid state of the box.
+ *
+ *        Get the solid state of the box.
+ */
+touchgfx::Rect touchgfx::BoxWithBorder::getSolidRect() const
+{
+	return (alpha != 255) ? touchgfx::Rect() : touchgfx::Rect(0, 0, getWidth(), getHeight());
+}
+
+/**
+ * @fn void BoxWithBorder::draw(const Rect& area) const;
+ *
+ * @brief Draws the box.
+ *
+ *        Draws the Box.
+ *
+ * @param area The rectangle to draw, with coordinates relative to the containing layer.
+ */
+void touchgfx::BoxWithBorder::draw(const touchgfx::Rect& invalidatedArea) const
+{
+	touchgfx::Rect absolute = getAbsoluteRect();
+	touchgfx::colortype newColor;
+	touchgfx::colortype backColor;
+	bool inBorder;
+	uint8_t out_r, out_g, out_b;
+	uint16_t *framebuffer = touchgfx::HAL::getInstance()->lockFrameBuffer();
 
 	for (int y = invalidatedArea.y; y < invalidatedArea.bottom(); y++)
 	{
@@ -48,10 +88,10 @@ void touchgfx::BoxWithBorder::draw(const touchgfx::Rect& invalidatedArea) const
 			// Manage border color
 			if (border != 0)
 			{
-				if ((y < (invalidatedArea.y+ border)) || \
-					(y >= (invalidatedArea.bottom() - border)) || \
-					(x < (invalidatedArea.x + border)) || \
-					(x >= (invalidatedArea.right() - border)))
+				if ((y < (border)) || \
+					(y >= (rect.height - border)) || \
+					(x < (border)) || \
+					(x >= (rect.width - border)))
 				{
 					inBorder = true;
 					newColor = borderColor;
@@ -62,15 +102,15 @@ void touchgfx::BoxWithBorder::draw(const touchgfx::Rect& invalidatedArea) const
 			{
 				if (fillMode != GRADIENT_H)
 				{
-					out_r = ((color >> 11)  * (x - invalidatedArea.x) + (endColor >> 11) * (invalidatedArea.right() - x)) / (invalidatedArea.right() - invalidatedArea.x);
-					out_g = (((color >> 5) & ((1u << 6) - 1))  * (x - invalidatedArea.x) + ((endColor >> 5) & ((1u << 6) - 1)) * (invalidatedArea.right() - x)) / (invalidatedArea.right() - invalidatedArea.x);
-					out_b = ((color & ((1u << 5) - 1))  * (x - invalidatedArea.x) + (endColor & ((1u << 5) - 1)) * (invalidatedArea.right() - x)) / (invalidatedArea.right() - invalidatedArea.x);
+					out_r = ((color >> 11)  * (x - 0) + (endColor >> 11) * (rect.width - x)) / (rect.width - 0);
+					out_g = (((color >> 5) & ((1u << 6) - 1))  * (x - 0) + ((endColor >> 5) & ((1u << 6) - 1)) * (rect.width - x)) / (rect.width - 0);
+					out_b = ((color & ((1u << 5) - 1))  * (x - 0) + (endColor & ((1u << 5) - 1)) * (rect.width - x)) / (rect.width - 0);
 				}
 				else
 				{
-					out_r = ((color >> 11)  * (y - invalidatedArea.y) + (endColor >> 11) * (invalidatedArea.bottom() - y)) / (invalidatedArea.bottom() - invalidatedArea.y);
-					out_g = (((color >> 5) & ((1u << 6) - 1))  * (y - invalidatedArea.y) + ((endColor >> 5) & ((1u << 6) - 1)) * (invalidatedArea.bottom() - y)) / (invalidatedArea.bottom() - invalidatedArea.y);
-					out_b = ((color & ((1u << 5) - 1))  * (y - invalidatedArea.y) + (endColor & ((1u << 5) - 1)) * (invalidatedArea.bottom() - y)) / (invalidatedArea.bottom() - invalidatedArea.y);
+					out_r = ((color >> 11)  * (y - 0) + (endColor >> 11) * (rect.height - y)) / (rect.height - 0);
+					out_g = (((color >> 5) & ((1u << 6) - 1))  * (y - 0) + ((endColor >> 5) & ((1u << 6) - 1)) * (rect.height - y)) / (rect.height - 0);
+					out_b = ((color & ((1u << 5) - 1))  * (y - 0) + (endColor & ((1u << 5) - 1)) * (rect.height - y)) / (rect.height - 0);
 				}
 				newColor = ((out_r << 11) | (out_g << 5) | out_b);
 			}
